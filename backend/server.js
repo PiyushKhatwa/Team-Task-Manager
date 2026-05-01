c1ed1a9f2224d0f363595534455dd78339a80eda
Piyush Khatwa <piyushkhatwa8888@gmail.com>
2026-05-01 23:30:30 +0530

    Add root route

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,7 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true,
-}));
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
